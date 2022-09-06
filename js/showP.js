29c604057ea97be51c4a8e6ded06442eb60b74e4
Carlos Zelada <106646303+JeezisPope@users.noreply.github.com>
2022-09-05 21:54:36 -0500

    Add files via upload

---
+++ js/showP.js
@@ -0,0 +1,34 @@
+function mostrar(){
+    var tipo = document.getElementById("password");
+    if(tipo.type == "password"){
+        tipo.type = "text";
+    }else{
+        tipo.type = "password";
+    }
+
+    var qlq = document.getElementById("boton");
+    if(qlq.textContent == "Show Password"){
+        qlq.textContent = "Hide Password"
+    } else{
+        qlq.textContent = "Show Password"
+    }
+}
+
+function validar(){
+    var tipo = document.getElementById("password");
+    var validacion = document.getElementById("Sign");
+    var fono = document.getElementById("cel")
+
+
+
+    if(tipo.textContent == " "){
+        alert("HUEVOS");
+    }else if(fono.length > 8){
+        alert("Digitar un numero de 8 Digitos");
+    }else{
+        alert("Ha iniciado sesion")
+    }
+
+}
+
+
